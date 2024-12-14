0a8952e3cf151c5a6584c3dd899db764d2b53034
Maheshkumar1510 <maheshmaahi151098@gmail.com>
2024-12-14 18:11:24 +0530

    Add files via upload

---
+++ src/main/java/org/mentorbridge/EmployeeApplication.java
@@ -0,0 +1,15 @@
+package org.mentorbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
